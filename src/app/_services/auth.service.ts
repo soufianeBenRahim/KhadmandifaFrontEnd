922099c90b78dd6bf853eca74c42f99ad8bf37d1
soufianeBenRahimMaison <Xpert.rahim.soufiane@gmail.com>
2020-03-04 06:34:33 +0100

    first commit

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'http://localhost:8080/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(credentials) {
+    return this.http.post(AUTH_API + 'login',credentials,{observe:'response'} );
+  }
+
+  register(user): Observable<any> {
+    return this.http.post(AUTH_API + 'register', {
+      username: user.username,
+      email: user.email,
+      password: user.password
+    },httpOptions);
+  }
+}
